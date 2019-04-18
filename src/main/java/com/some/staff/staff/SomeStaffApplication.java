e2631dd4ac5cabf9574ddedb4d323e797a41919f
Vladislav_K <v.kurashkevich@gmail.com>
2019-04-18 11:48:04 +0300

    init some staff

---
+++ src/main/java/com/some/staff/staff/SomeStaffApplication.java
@@ -0,0 +1,12 @@
+package com.some.staff.staff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SomeStaffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomeStaffApplication.class, args);
+    }
+
+}
