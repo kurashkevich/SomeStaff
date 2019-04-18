bf77da5b362dbeb696c08944a7fd255ce0c7bb6a
Vladislav_K <v.kurashkevich@gmail.com>
2019-04-18 12:25:24 +0300

    test

--- archunit/src/main/java/com/example/archunit/ArchUnitApplication.java
+++ archunit/src/main/java/com/example/archunit/ArchUnitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArchUnitApplication {
 
     public static void main(String[] args) {
+        System.out.println("Arch started");
         SpringApplication.run(ArchUnitApplication.class, args);
     }
 
